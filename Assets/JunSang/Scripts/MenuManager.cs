7c2dc920a59f8aa25d0e51ec326bcff7c19b972d
Junsang <kimnora070623@gmail.com>
2024-01-14 01:22:50 +0900

    ButtonClick/Sound, MenuButton Update
    20240114

--- Assets/JunSang/Scripts/MenuManager.cs
+++ Assets/JunSang/Scripts/MenuManager.cs
@@ -1,18 +1,18 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.MPE;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Jobs;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] _menuList; 
     [SerializeField] private Button[] _firstSelectButton;
+    [SerializeField] private Slider[] _firstSelectSlider;
     [SerializeField] private int _Menuindex = 0; 
     [SerializeField] private int _Buttonindex = 0; 
+    [SerializeField] private int _Sliderindex = -1;
     // Start is called before the first frame update
 
     void Awake(){
@@ -21,34 +21,45 @@ public class MenuManager : MonoBehaviour
     }
     void Start()
     {
-        SelectedMenu(_Menuindex, _Buttonindex);
+        SelectedMenu(_Menuindex, _Buttonindex, _Sliderindex);
     }
 
-    public void SelectedMenu(int MenuIndex, int ButtonIndex){
+    public void SelectedMenu(int MenuIndex, int ButtonIndex, int SliderIndex){
         foreach(GameObject menu in _menuList){ 
             menu.SetActive(false);
         }
         _menuList[MenuIndex].SetActive(true); 
-        _firstSelectButton[ButtonIndex].Select(); 
+        if(_Buttonindex >= 0)
+            _firstSelectButton[ButtonIndex].Select(); 
+        if(_Sliderindex >= 0){
+            _firstSelectSlider[SliderIndex].Select();
+        }
     }
 
     public void MenuButton(Button button){
         if(button.name == "Start_Button"){ 
             _Menuindex = 0;                       
             _Buttonindex = 0;
+            _Sliderindex = -1;
+            AudioManager.Instance.PlaySFX("ButtonClick");
             StartCoroutine(WaitForSecond());  
         }
         else if(button.name == "OptionMenu_Button"){
             _Menuindex = 1;                      
-            _Buttonindex = 3;
+            _Buttonindex = -1;
+            _Sliderindex = 0;
+            AudioManager.Instance.PlaySFX("ButtonClick");
             StartCoroutine(WaitForSecond());
         }
         else if(button.name == "BackToMainMenu_Button"){
             _Menuindex = 0;                      
             _Buttonindex = 0;
+            _Sliderindex = -1;
+            AudioManager.Instance.PlaySFX("ButtonClick");
             StartCoroutine(WaitForSecond());
         }
         else if(button.name == "ExitMenu_Button"){
+            AudioManager.Instance.PlaySFX("ButtonClick");
             Debug.Log("게임 종료됨"); 
             Application.Quit();
         }
@@ -56,8 +67,7 @@ public class MenuManager : MonoBehaviour
 
     IEnumerator WaitForSecond(){              
         yield return new WaitForSeconds(.3f); 
-        SelectedMenu(_Menuindex, _Buttonindex);         
+        SelectedMenu(_Menuindex, _Buttonindex, _Sliderindex);         
     }
-
 
 }
